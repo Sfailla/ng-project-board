ebe326b9180918b948e666b4b41dc9c8c130c3b6
sfailla <sfailla1983@gmail.com>
2022-09-02 09:34:52 -0400

    comment out default project code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  `,
+  styles: [``],
 })
 export class AppComponent {
   title = 'ng-project-board';
