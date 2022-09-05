960b4fb0b09cf790944e7b949e5afe62404d7243
sfailla <sfailla1983@gmail.com>
2022-09-05 18:06:07 -0400

    add color variable

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,90 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-header',
-  template: `
-    <header class="header">
-      <div class="header__post"></div>
-      <div class="header__toolbar">
-        <div class="header__logo">
-          <h3 class="header__logo--text">Project Board</h3>
-          <!-- <img src="assets/images/logo.png" alt="logo" /> -->
-        </div>
-        <div class="header__profile">
-          <div class="header__profile-photo">
-            <!-- <img src="assets/images/profile.jpg" alt="profile" /> -->
-          </div>
-          <div class="header__profile-info">
-            <span class="header__profile-name">User 1</span>
-          </div>
-        </div>
-      </div>
-    </header>
-  `,
-  styles: [
-    `
-      @import '../../../styles/abstracts';
+	selector: 'app-header',
+	template: `
+		<ion-header class="ion-no-border header" collapse="fade">
+			<ion-toolbar class="header__toolbar">
+				<div class="header__profile" slot="end">
+					<div class="header__profile-photo">
+						<!-- <img src="assets/images/profile.jpg" alt="profile" /> -->
+					</div>
+					<div class="header__profile-info">
+						<span class="header__profile-name">User 1</span>
+					</div>
+				</div>
+				<div class="header__logo">
+					<ion-title class="header__logo--text">Project Board</ion-title>
+					<!-- <img src="assets/images/logo.png" alt="logo" /> -->
+				</div>
+			</ion-toolbar>
+		</ion-header>
+	`,
+	styles: [
+		`
+			@import '../../../styles/abstracts';
 
-      .header {
-        height: var(--header-height);
-        display: grid;
-        grid-template-columns: 18rem 1fr;
+			.header {
+				width: 100%;
+				height: var(--header-height);
+				border-bottom: $primary-border;
 
-        &__post {
-          height: var(--header-height);
-          background-color: $primary-color;
-        }
+				&__toolbar {
+					width: 100%;
+					height: 100%;
+					@include flex(space-between);
+					padding: 0 20px;
+					background-color: #fff;
+				}
 
-        &__toolbar {
-          width: 100%;
-          @include flex(space-between, center);
-          padding: 0 20px;
-          background-color: #fff;
-          border-bottom: 1px solid #e5e5e5;
-        }
+				&__logo {
+					&--text {
+						font-size: 1.5rem;
+						text-transform: uppercase;
+						color: var(--ion-color-primary);
+					}
+				}
 
-        &__logo {
-          @include flex(flex-start);
+				&__logo img {
+					width: 100px;
+				}
 
-          &--text {
-            font-size: 1.5rem;
-            text-transform: uppercase;
-            color: $primary-color;
-          }
-        }
+				&__profile {
+					@include flex(flex-start);
+				}
 
-        &__logo img {
-          width: 100px;
-        }
+				&__profile-photo {
+					width: 3.5rem;
+					height: 3.5rem;
+					border-radius: 50%;
+					background-color: #e5e5e5;
+				}
 
-        &__profile {
-          @include flex(flex-start);
-        }
+				&__profile-info {
+					@include flex(flex-start);
+					margin-left: 10px;
+				}
 
-        &__profile-photo {
-          width: 40px;
-          height: 40px;
-          border-radius: 50%;
-          background-color: #ccc;
-        }
-
-        &__profile-info {
-          @include flex(flex-start);
-          margin-left: 10px;
-        }
-
-        &__profile-name {
-          font-size: 14px;
-          font-weight: bold;
-          color: $primary-color;
-          text-transform: uppercase;
-        }
-      }
-    `,
-  ],
+				&__profile-name {
+					font-size: 14px;
+					font-weight: bold;
+					color: $primary-color;
+					text-transform: uppercase;
+				}
+			}
+		`
+	]
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+	constructor() {}
 
-  ngOnInit(): void {}
+	ngOnInit(): void {}
 }
