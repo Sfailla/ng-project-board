e8a159b2e3aa8f960269bc5612a8e394c8cdc9ce
sfailla <sfailla1983@gmail.com>
2022-09-05 14:24:13 -0400

    add app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <app-header></app-header>
-    <app-side-menu></app-side-menu>
-    <router-outlet></router-outlet>
-  `,
-  styles: [``],
+	selector: 'app-root',
+	template: `
+		<ion-app>
+			<div class="app-layout">
+				<app-side-menu></app-side-menu>
+				<div class="app-layout__content">
+					<app-header></app-header>
+					<ion-router-outlet></ion-router-outlet>
+				</div>
+			</div>
+		</ion-app>
+	`,
+	styles: [``]
 })
 export class AppComponent {
-  title = 'ng-project-board';
+	title = 'ng-project-board'
 }
