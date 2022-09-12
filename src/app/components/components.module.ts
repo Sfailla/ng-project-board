d5972d9a3fdc600feaf04f00f5c48232bee0658a
sfailla <sfailla1983@gmail.com>
2022-09-11 20:27:36 -0400

    add logo component and styles

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common'
 import { HeaderComponent } from './header/header.component'
 import { SideMenuComponent } from './side-menu/side-menu.component'
 import { IonicModule } from '@ionic/angular'
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
+import { LogoComponent } from './logo/logo.component'
 
 @NgModule({
-	declarations: [HeaderComponent, SideMenuComponent],
+	declarations: [HeaderComponent, SideMenuComponent, LogoComponent],
 	imports: [CommonModule, IonicModule, RouterModule],
 	exports: [HeaderComponent, SideMenuComponent]
 })
---
+++ src/app/components/logo/logo.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+	selector: 'app-logo',
+	template: `
+		<span class="logo"></span>
+		<span class="logo-inner"></span>
+	`,
+	styles: [
+		`
+			@import '../../../styles/abstracts';
+
+			.logo {
+				width: 2.5rem;
+				height: 2.5rem;
+				position: relative;
+				display: block;
+				@include flex();
+				background-color: $primary-color;
+				-webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+				clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+
+				&::before {
+					content: '';
+					display: block;
+					height: 2rem;
+					width: 2rem;
+					background-color: white;
+					-webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+					clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+				}
+
+				&::after {
+					content: '';
+					width: 1.5rem;
+					height: 1.5rem;
+					display: block;
+					position: absolute;
+					top: 50%;
+					left: 50%;
+					transform: translate(-50%, -50%);
+					background-color: $primary-color;
+					-webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+					clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+				}
+			}
+
+			.logo-inner {
+				width: 1rem;
+				height: 1rem;
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				display: block;
+				background-color: #ffffff;
+				-webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+				clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+
+				&::after {
+					content: '';
+					width: 0.5rem;
+					height: 0.5rem;
+					position: absolute;
+					top: 50%;
+					left: 50%;
+					transform: translate(-50%, -50%);
+					display: block;
+					background-color: $primary-color;
+					-webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+					clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+				}
+			}
+		`
+	]
+})
+export class LogoComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
