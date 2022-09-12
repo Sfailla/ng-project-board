374a13149310eed118fda72f160eacf1971e038b
sfailla <sfailla1983@gmail.com>
2022-09-12 17:21:08 -0400

    add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { IonicModule } from '@ionic/angular'
-import { ComponentsModule } from './components/components.module'
+import { SharedModule } from './shared/shared.module'
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, IonicModule.forRoot({}), ComponentsModule],
+	imports: [BrowserModule, AppRoutingModule, IonicModule.forRoot({}), SharedModule],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common'
 import { HeaderComponent } from './header/header.component'
 import { SideMenuComponent } from './side-menu/side-menu.component'
 import { IonicModule } from '@ionic/angular'
-import { RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router'
 import { LogoComponent } from './logo/logo.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
 	declarations: [HeaderComponent, SideMenuComponent, LogoComponent],
-	imports: [CommonModule, IonicModule, RouterModule],
+	imports: [CommonModule, IonicModule, RouterModule, SharedModule],
 	exports: [HeaderComponent, SideMenuComponent]
 })
 export class ComponentsModule {}
