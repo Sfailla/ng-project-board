323d8c10aa6127396ac0be2243837712cfe5e175
sfailla <sfailla1983@gmail.com>
2022-09-18 19:54:54 -0400

    add register and logout methods. fix imports for codegen types

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core'
 import { Apollo, MutationResult } from 'apollo-angular'
 import { BehaviorSubject, map, shareReplay } from 'rxjs'
-import { User, UserWithToken } from 'src/generated/graphql'
+import { User, UserWithToken } from 'src/generated/types.graphql-gen'
 import { AuthTokenService } from './auth-token.service'
 import { ApolloQueryResult } from '@apollo/client/core/types'
 import { Router } from '@angular/router'
 import { Me, Routes } from 'src/types'
-import { LoginDocument as loginMutation, MeDocument as meQuery } from 'src/generated/graphql'
+import { MeDocument as meQuery } from 'src/generated/queries/index.graphql-gen'
+import {
+	LoginDocument as loginMutation,
+	RegisterDocument as registerMutation
+} from 'src/generated/mutations/index.graphql-gen'
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -26,24 +30,6 @@ export class AuthService {
 		return this.currentUser$.value
 	}
 
-	async login(email: string, password: string): Promise<void> {
-		this.apollo
-			.mutate<UserWithToken>({
-				mutation: loginMutation,
-				fetchPolicy: 'no-cache',
-				variables: { email, password }
-			})
-			.subscribe(({ data, loading }: MutationResult) => {
-				const { user, token } = data?.login
-
-				this.currentUser$.next(user)
-				this.tokenService.saveToken(token)
-
-				this.router.navigate([Routes.Dashboard])
-			}),
-			shareReplay(1)
-	}
-
 	getCurrentSession() {
 		if (!this.isAuthenticated()) {
 			return
@@ -52,13 +38,68 @@ export class AuthService {
 		this.apollo
 			.watchQuery<Me>({
 				query: meQuery,
-				fetchPolicy: 'no-cache'
+				fetchPolicy: 'network-only'
 			})
 			.valueChanges.subscribe(({ data }: ApolloQueryResult<Me>) => {
 				const { me } = data
 
 				this.currentUser$.next(me)
+				this.tokenService.saveUser(me)
 			}),
 			shareReplay(1)
+	}
+
+	async register(
+		username: string,
+		email: string,
+		password: string,
+		confirmPassword: string
+	): Promise<void> {
+		if (password !== confirmPassword) {
+			throw new Error('🔐 Passwords do not match')
+		}
+
+		this.apollo
+			.mutate<UserWithToken>({
+				mutation: registerMutation,
+				fetchPolicy: 'network-only',
+				variables: { username, email, password }
+			})
+			.subscribe(({ data }: MutationResult) => {
+				const { user, token } = data?.register
+
+				this.currentUser$.next(user)
+				this.tokenService.saveToken(token)
+				this.tokenService.saveUser(user)
+
+				this.router.navigate([Routes.Dashboard])
+			}),
+			shareReplay(1)
+	}
+
+	async login(email: string, password: string): Promise<void> {
+		this.apollo
+			.mutate<UserWithToken>({
+				mutation: loginMutation,
+				fetchPolicy: 'network-only',
+				variables: { email, password }
+			})
+			.subscribe(({ data }: MutationResult) => {
+				const { user, token } = data?.login
+
+				this.currentUser$.next(user)
+				this.tokenService.saveToken(token)
+				this.tokenService.saveUser(user)
+
+				this.router.navigate([Routes.Dashboard])
+			}),
+			shareReplay(1)
+	}
+
+	logout() {
+		this.tokenService.destroySession()
+		this.currentUser$.next(null)
+		this.apollo.client.resetStore()
+		// this.router.navigate([Routes.Login])
 	}
 }
