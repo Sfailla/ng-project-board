3caa93b8dc6f93cb7bd2d292ad4ae2f6777f7fe5
sfailla <sfailla1983@gmail.com>
2022-09-18 09:23:52 -0400

    add logic to check for auth session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { Routes } from '../types'
+import { AuthTokenService } from './auth/auth-token.service'
+import { AuthService } from './auth/auth.service'
 
 @Component({
 	selector: 'app-root',
@@ -9,6 +13,20 @@ import { Component } from '@angular/core'
 	`,
 	styles: [``]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'ng-project-board'
+
+	constructor(
+		private authService: AuthService,
+		private tokenService: AuthTokenService,
+		private router: Router
+	) {}
+
+	ngOnInit() {
+		if (this.tokenService.getUser()) {
+			this.authService.getCurrentSession()
+		} else {
+			this.router.navigate([Routes.Login])
+		}
+	}
 }
