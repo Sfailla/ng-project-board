f61fafb4099a2052f02d7e170ceb7c1b76759f93
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-20 10:40:17 -0500

    initialize styles folder and scss scaffolding

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [],
-  template: ` <h1>Register Component Works</h1> `,
-  styles: [``],
+  template: `
+    <h1>Register Route Works!</h1>
+  `,
+  styles: [``]
 })
-export class RegisterComponent {
-  // Your code here
-}
+export class RegisterComponent {}
