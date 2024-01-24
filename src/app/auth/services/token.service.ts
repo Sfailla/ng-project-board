50e1eca83e5d10ebacf0f34ab51e15711c34bd36
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-24 12:20:35 -0500

    add token service

---
+++ src/app/auth/services/token.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core'
+import { environment } from '../../../environments/environment.development'
+import { User } from '../../../generated/types.graphql-gen'
+
+const TOKEN_KEY = environment.authTokenKey
+const USER_KEY = environment.authUserKey
+
+@Injectable({ providedIn: 'root' })
+export class TokenService {
+  destroySession(): void {
+    window.sessionStorage.clear()
+  }
+
+  saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY)
+    window.sessionStorage.setItem(TOKEN_KEY, token)
+  }
+
+  saveUser(user: User): void {
+    window.sessionStorage.removeItem(USER_KEY)
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user))
+  }
+
+  saveUserAndToken(user: User, token: string): void {
+    this.saveToken(token)
+    this.saveUser(user)
+  }
+
+  public getToken(): string {
+    return window.sessionStorage.getItem(TOKEN_KEY) || ''
+  }
+
+  public getUser(): User | null {
+    const user = window.sessionStorage.getItem(USER_KEY)
+    return user ? JSON.parse(user) : null
+  }
+}
