aef44c986f8c80c4b7b840172fb661ead23ca9b5
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-24 12:16:27 -0500

    add codegen file

---
+++ codegen.ts
@@ -0,0 +1,36 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'http://localhost:4000/graphql',
+  documents: 'src/**/*.graphql',
+  generates: {
+    'src/generated/types.graphql-gen.ts': {
+      plugins: ['typescript']
+    },
+    'src/generated': {
+      preset: 'near-operation-file',
+      presetConfig: {
+        extension: '.graphql-gen.ts',
+        baseTypesPath: 'types.graphql-gen.ts'
+      },
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-resolvers',
+        'typescript-apollo-angular'
+      ],
+      config: {
+        addExplicitOverride: true
+      }
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection']
+    }
+  },
+  config: {
+    addExplicitOverride: true
+  }
+}
+
+export default config
