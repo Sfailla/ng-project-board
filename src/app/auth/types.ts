2fc38aea7735928aa0f9e6c956770e929b09efb1
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-24 12:19:14 -0500

    add AuthUserInput type

--- src/app/auth/types.ts
+++ src/app/auth/types.ts
@@ -2,3 +2,10 @@ export enum AuthTitles {
   LOGIN = 'Sign Into',
   REGISTER = 'Sign Up For'
 }
+
+export interface AuthUserInput {
+  username?: string
+  email: string
+  password: string
+  confirmPassword?: string
+}
