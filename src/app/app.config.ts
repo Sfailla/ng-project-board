7a6fd07470064470af2fb6a765dd9daee70f9596
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-24 12:18:08 -0500

    add graphql provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
+import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes';
-import { provideIonicAngular } from '@ionic/angular/standalone';
+import { routes } from './app.routes'
+import { provideIonicAngular } from '@ionic/angular/standalone'
+import { HttpClientModule, provideHttpClient } from '@angular/common/http'
+import { graphqlProvider } from './graphql.provider'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideIonicAngular({})]
-};
+  providers: [
+    provideRouter(routes),
+    provideIonicAngular({}),
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+    graphqlProvider
+  ]
+}
