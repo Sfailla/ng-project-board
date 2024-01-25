93191f1ab980785c1e323dbad6e8576a116460e8
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-24 20:18:57 -0500

    create dashboard routes

---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,27 @@
+import { DashboardComponent } from './dashboard.component'
+
+export const DASHBOARD_ROUTES = [
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: []
+    // children: [
+    //   {
+    //     path: 'home',
+    //     loadChildren: () => import('../home/home-routes').then(m => m.HOME_ROUTES)
+    //   },
+    //   {
+    //     path: ':id/tasks',
+    //     loadChildren: () => import('../tasks/task-routes').then(m => m.TASK_ROUTES)
+    //   },
+    //   {
+    //     path: 'projects',
+    //     loadChildren: () => import('../projects/project-routes').then(m => m.PROJECT_ROUTES)
+    //   }
+    //   // {
+    //   // 	path: 'settings',
+    //   // 	loadChildren: () => import('../settings/settings.module').then(m => m.SettingsModule)
+    //   // },
+    // ]
+  }
+]
