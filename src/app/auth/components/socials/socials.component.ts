7d1d33e44447045f0b55a861f2bd714e50747444
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-24 19:59:56 -0500

    create socials component

--- src/app/auth/components/socials/socials.component.ts
+++ src/app/auth/components/socials/socials.component.ts
@@ -6,7 +6,7 @@ import { IonicModule } from '@ionic/angular'
   standalone: true,
   imports: [IonicModule],
   template: `
-    <div class="login__socials">
+    <div class="socials">
       <a class="circle-link">
         <ion-icon name="logo-google"></ion-icon>
       </a>
@@ -21,6 +21,47 @@ import { IonicModule } from '@ionic/angular'
   styles: [
     `
       @import '../../../styles/abstracts';
+
+      .socials {
+        width: 100%;
+        max-width: 20rem;
+        margin: 0 auto;
+        @include flex(space-around);
+
+        & .circle-link {
+          height: 40px;
+          width: 40px;
+          border-radius: 40px;
+          margin: 8px;
+          background-color: white;
+          @include flex();
+          cursor: pointer;
+          outline: none;
+          appearance: none;
+          box-shadow: 0 1px 2px 1px rgba(0, 0, 0, 0.15);
+          transition: 0.3s ease-out;
+
+          &:hover {
+            transform: translateY(-0.2rem);
+          }
+
+          & ion-icon {
+            font-size: 1.8rem;
+
+            &[name='logo-google'] {
+              color: #db4437;
+            }
+
+            &[name='logo-github'] {
+              color: #000;
+            }
+
+            &[name='logo-apple'] {
+              color: #000;
+            }
+          }
+        }
+      }
     `
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
