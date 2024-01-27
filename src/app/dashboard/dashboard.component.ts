07e84ef23c810f0784bf16f9dac82199697ec085
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-27 08:37:22 -0500

    add components for dashboard layout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { IonicModule } from '@ionic/angular'
+import { SideMenuComponent, HeaderComponent } from './components'
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [IonicModule, RouterOutlet],
+  imports: [IonicModule, RouterOutlet, SideMenuComponent, HeaderComponent],
   template: `
-    <h1>Dashboard Works!</h1>
-    <ion-router-outlet id="main"></ion-router-outlet>
+    <div class="app-layout">
+      <app-side-menu></app-side-menu>
+      <ion-content class="app-layout__content page-container" color="light">
+        <app-header></app-header>
+        <!-- <app-settings-menu></app-settings-menu> -->
+        <ion-router-outlet id="main" class="router-outlet"></ion-router-outlet>
+      </ion-content>
+    </div>
   `,
   styles: [``],
   changeDetection: ChangeDetectionStrategy.OnPush
