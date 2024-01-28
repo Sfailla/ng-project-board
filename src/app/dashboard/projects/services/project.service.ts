ab98e0d9c91de8b9468ee00617e07d09e753d63f
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-28 15:40:18 -0500

    add project service

---
+++ src/app/dashboard/projects/services/project.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, inject } from '@angular/core'
+import { Apollo } from 'apollo-angular'
+import { GetProjectsDocument } from '../../../../generated/queries/index.graphql-gen'
+import { map } from 'rxjs/internal/operators/map'
+import { Project } from '../../../../generated/types.graphql-gen'
+import { LocalStorageService } from '../../../shared/services'
+
+@Injectable({ providedIn: 'root' })
+export class ProjectService {
+  apollo: Apollo = inject(Apollo)
+  storageService: LocalStorageService = inject(LocalStorageService)
+
+  setProjectId(projectId: string): void {
+    this.storageService.setItem('project-id', projectId)
+  }
+
+  getProjects() {
+    return this.apollo
+      .watchQuery<{ getProjects: Project[] }>({ query: GetProjectsDocument })
+      .valueChanges.pipe(
+        map(({ data }) => {
+          return data.getProjects
+        })
+      )
+  }
+}
