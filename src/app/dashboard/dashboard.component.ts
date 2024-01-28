8f8e8c5423fb0c1be919e6a268649de1a34fc4cc
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-28 13:23:13 -0500

    Add app settings component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { IonicModule } from '@ionic/angular'
-import { SideMenuComponent, HeaderComponent } from './components'
+import { SideMenuComponent, HeaderComponent, SettingsMenuComponent } from './components'
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [IonicModule, RouterOutlet, SideMenuComponent, HeaderComponent],
+  imports: [IonicModule, RouterOutlet, SideMenuComponent, HeaderComponent, SettingsMenuComponent],
   template: `
     <div class="app-layout">
       <app-side-menu></app-side-menu>
       <ion-content class="app-layout__content page-container">
         <app-header></app-header>
-        <!-- <app-settings-menu></app-settings-menu> -->
+        <app-settings-menu></app-settings-menu>
         <ion-router-outlet id="main" class="router-outlet"></ion-router-outlet>
       </ion-content>
     </div>
