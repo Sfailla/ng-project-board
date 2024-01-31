64998c97e0581bbf90e41d584f6b6d6e4e100c92
Steven-failla_mmcgh <steven.failla@nera.com>
2024-01-31 07:26:58 -0500

    add create project component

--- src/app/dashboard/projects/components/create-project/create-project.component.ts
+++ src/app/dashboard/projects/components/create-project/create-project.component.ts
@@ -2,59 +2,59 @@ import { Component } from '@angular/core'
 import { IonicModule } from '@ionic/angular'
 
 @Component({
-	selector: 'app-create-project',
-	standalone: true,
-	imports: [IonicModule],
-	template: `
-		<div class="page-container create-project">
-			<ion-card class="create-project__card">
-				<ion-grid>
-					<ion-row>
-						<ion-col>
-							<ion-card-header class="create-project__card-header">
-								<ion-card-title>New Project</ion-card-title>
-							</ion-card-header>
+  selector: 'app-create-project',
+  standalone: true,
+  imports: [IonicModule],
+  template: `
+    <div class="page-container create-project">
+      <ion-card class="create-project__card">
+        <ion-grid>
+          <ion-row>
+            <ion-col>
+              <ion-card-header class="create-project__card-header">
+                <ion-card-title>New Project</ion-card-title>
+              </ion-card-header>
 
-							<form action="">
-								<ion-card-content class="create-project__card-content">
-									<ion-item>
-										<ion-label position="floating">Project Name</ion-label>
-										<ion-input type="text"></ion-input>
-									</ion-item>
+              <form action="">
+                <ion-card-content class="create-project__card-content">
+                  <ion-item>
+                    <ion-label position="floating">Project Name</ion-label>
+                    <ion-input type="text"></ion-input>
+                  </ion-item>
 
-									<ion-item>
-										<ion-label position="floating">Project Description</ion-label>
-										<ion-textarea></ion-textarea>
-									</ion-item>
-								</ion-card-content>
-							</form>
-						</ion-col>
-						<ion-col>placeholder</ion-col>
-					</ion-row>
-				</ion-grid>
-			</ion-card>
-		</div>
-	`,
-	styles: [
-		`
-			@import '../../../styles/abstracts';
+                  <ion-item>
+                    <ion-label position="floating">Project Description</ion-label>
+                    <ion-textarea></ion-textarea>
+                  </ion-item>
+                </ion-card-content>
+              </form>
+            </ion-col>
+            <ion-col>placeholder</ion-col>
+          </ion-row>
+        </ion-grid>
+      </ion-card>
+    </div>
+  `,
+  styles: [
+    `
+      @import '../../../../styles/abstracts';
 
-			.create-project {
-				&__card {
-				}
+      .create-project {
+        &__card {
+        }
 
-				&__card-header {
-					padding: 1.6rem;
+        &__card-header {
+          padding: 1.6rem;
 
-					& ion-card-title {
-						font-size: 3.2rem;
-					}
-				}
+          & ion-card-title {
+            font-size: 3.2rem;
+          }
+        }
 
-				&__card-content {
-				}
-			}
-		`
-	]
+        &__card-content {
+        }
+      }
+    `
+  ]
 })
 export class CreateProjectComponent {}
