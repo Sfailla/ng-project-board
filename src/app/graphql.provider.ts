f52f63107b341befac5e851eef4918c896a1c2fb
Steven-failla_mmcgh <steven.failla@nera.com>
2024-02-11 08:09:05 -0500

    overhaul and refactor of graphql.provider file

--- src/app/graphql.provider.ts
+++ src/app/graphql.provider.ts
@@ -1,27 +1,81 @@
 import { Apollo, APOLLO_OPTIONS } from 'apollo-angular'
 import { HttpLink } from 'apollo-angular/http'
-import { inject, Provider } from '@angular/core'
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core'
-import { HttpHeaders } from '@angular/common/http'
+import { onError } from '@apollo/client/link/error'
+import { ApplicationConfig, inject } from '@angular/core'
+import { setContext } from '@apollo/client/link/context'
+import { ApolloClientOptions, ApolloLink, InMemoryCache } from '@apollo/client/core'
 import { environment } from '../environments/environment.development'
+import { Router } from '@angular/router'
+import { DOCUMENT } from '@angular/common'
+import { LocalStorageKeys, Routes, Messages, ErrorMessages } from './types'
 
 const uri = environment.graphqlURI
+
+const clearAuthUserAndToken = () => {
+  localStorage.removeItem(LocalStorageKeys.AUTH_TOKEN)
+  localStorage.removeItem(LocalStorageKeys.AUTH_USER)
+}
+
 export function apolloOptionsFactory(): ApolloClientOptions<unknown> {
+  const document = inject(DOCUMENT)
+  const localStorage = document.defaultView?.localStorage
+  const token = localStorage?.getItem(LocalStorageKeys.AUTH_TOKEN) || null
+
+  const authLink = setContext((_, { headers }) => {
+    console.log({ token })
+
+    if (!token) return {}
+    else
+      return {
+        headers: {
+          ...headers,
+          'x-auth-token': token
+        }
+      }
+  })
+
+  const router = inject(Router)
+
+  const clearCredentialsAndNavigateToLogin = () => {
+    clearAuthUserAndToken()
+    router.navigate([Routes.LOGIN, { message: Messages.SESSION_EXPIRED }])
+  }
+
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message, locations, path }) => {
+        console.error(
+          `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${path}`
+        )
+      })
+
+      const firstError = graphQLErrors[0]
+
+      const errorMap: { [key: string]: () => void } = {
+        [ErrorMessages.JWT]: clearCredentialsAndNavigateToLogin,
+        [ErrorMessages.UNAUTHORIZED]: () => window.location.reload()
+      }
+
+      errorMap[firstError.message]?.()
+
+      if (networkError) {
+        console.log(networkError)
+        // setup route to error page with message
+        // router.navigate(['/error', { message: networkError.message }])
+      }
+    }
+  })
+
   const httpLink = inject(HttpLink)
-  const token = sessionStorage.getItem('auth-token') || ''
-  console.log({ token })
+  const link = ApolloLink.from([authLink, errorLink, httpLink.create({ uri })])
 
   return {
-    link: httpLink.create({
-      uri,
-      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'x-auth-token': token }),
-      withCredentials: true
-    }),
+    link,
     cache: new InMemoryCache()
   }
 }
 
-export const graphqlProvider: Provider[] = [
+export const graphqlProvider: ApplicationConfig['providers'] = [
   Apollo,
   {
     provide: APOLLO_OPTIONS,
