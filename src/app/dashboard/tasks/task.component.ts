a87362681a03e8bdf7c9401acc3bf1cf3dbbc940
Steven-failla_mmcgh <steven.failla@nera.com>
2024-02-11 08:23:40 -0500

    create task component

---
+++ src/app/dashboard/tasks/task.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { PageWrapperComponent } from '../projects/components/page-wrapper/page-wrapper.component'
+import { IonicModule } from '@ionic/angular'
+
+@Component({
+  standalone: true,
+  imports: [IonicModule, PageWrapperComponent],
+  selector: 'app-task',
+  template: `
+    <app-page-wrapper title="Tasks">
+      <h1>Tasks</h1>
+    </app-page-wrapper>
+  `,
+  styles: [``],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TaskComponent {}
