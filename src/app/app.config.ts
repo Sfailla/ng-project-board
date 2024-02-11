cbd038560403f3fbc792f90a9345605afdcee211
Steven-failla_mmcgh <steven.failla@nera.com>
2024-02-11 08:08:36 -0500

    comment out unnecessary providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,17 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core'
+import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
-
 import { routes } from './app.routes'
 import { provideIonicAngular } from '@ionic/angular/standalone'
-import { HttpClientModule, provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withFetch } from '@angular/common/http'
 import { graphqlProvider } from './graphql.provider'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideIonicAngular({}),
-    provideHttpClient(),
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withFetch()),
     graphqlProvider
+    // provideClientHydration(),
+    // importProvidersFrom(HttpClientModule),
   ]
 }
