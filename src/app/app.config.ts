1cbb2edd62205721f861beb6d1516fd240acb1a2
Steven-failla_mmcgh <steven.failla@nera.com>
2024-02-16 14:18:30 -0500

    add interceptor to providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,15 @@ import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { routes } from './app.routes'
 import { provideIonicAngular } from '@ionic/angular/standalone'
-import { provideHttpClient, withFetch } from '@angular/common/http'
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'
 import { graphqlProvider } from './graphql.provider'
+import { authInterceptor } from './auth/interceptors'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideIonicAngular({}),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
     graphqlProvider
   ]
 }
