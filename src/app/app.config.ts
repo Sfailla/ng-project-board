89028a05e0c28d6e8bf02415dd7ce4cf44fe4380
Steven-failla_mmcgh <steven.failla@nera.com>
2024-02-25 09:10:42 -0500

    remove unnecessary imports and providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,14 @@ import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { routes } from './app.routes'
 import { provideIonicAngular } from '@ionic/angular/standalone'
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'
+import { provideHttpClient, withFetch } from '@angular/common/http'
 import { graphqlProvider } from './graphql.provider'
-import { authInterceptor } from './auth/interceptors'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideIonicAngular({}),
-    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
+    provideHttpClient(withFetch()),
     graphqlProvider
   ]
 }
