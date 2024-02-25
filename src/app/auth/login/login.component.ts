c2dd39ea05385b61b4b4e7cea643b0910a4521cd
Steven-failla_mmcgh <steven.failla@nera.com>
2024-02-25 10:05:23 -0500

    remove getting message from route param

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,35 +1,33 @@
-import { Component, OnInit, inject } from '@angular/core'
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core'
 import { AuthComponent } from '../components/auth/auth.component'
 import { AuthTitles } from '../auth-types'
 import { ActivatedRoute } from '@angular/router'
+import { PageWrapperComponent, ToastMessageComponent } from '../../shared/components'
+import { ToastService } from '../../shared/services/toast/toast.service'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [AuthComponent],
+  imports: [AuthComponent, PageWrapperComponent, ToastMessageComponent],
   template: `
-    <div class="page-container">
+    <app-page-wrapper class="login">
       <app-auth [title]="AuthTitles.LOGIN"></app-auth>
-    </div>
+    </app-page-wrapper>
   `,
   styles: [
     `
       @import '../../styles/abstracts';
 
-      .page-container {
+      .login {
         @include flex();
         background-color: var(--ion-color-primary);
       }
     `
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   AuthTitles: typeof AuthTitles = AuthTitles
   route = inject(ActivatedRoute)
-
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      console.log({ message: params['message'], params })
-    })
-  }
+  toastService: ToastService = inject(ToastService)
 }
