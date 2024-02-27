cb8cdda69e6f27ca3ef7821008f7874853312b93
Steven-failla_mmcgh <steven.failla@nera.com>
2024-02-27 16:28:07 -0500

    update enum type for error message

--- src/app/graphql.provider.ts
+++ src/app/graphql.provider.ts
@@ -4,7 +4,7 @@ import { onError } from '@apollo/client/link/error'
 import { inject, Provider } from '@angular/core'
 import { ApolloClientOptions, ApolloLink, InMemoryCache } from '@apollo/client/core'
 import { environment } from '../environments/environment.development'
-import { LocalStorageKeys, Routes, Messages, ErrorMessages } from './shared-types'
+import { LocalStorageKeys, Routes, ErrorMessages } from './shared-types'
 import { NavController } from '@ionic/angular'
 import { ToastService } from './shared/services/toast/toast.service'
 import { HttpHeaders } from '@angular/common/http'
@@ -17,7 +17,7 @@ export function apolloOptionsFactory(): ApolloClientOptions<unknown> {
 
   const clearCredentialsAndNavigateToLogin = () => {
     tokenService.destroySession()
-    toastService.setToastMessage({ variant: 'error', message: Messages.SESSION_EXPIRED })
+    toastService.setToastMessage({ variant: 'error', message: ErrorMessages.SESSION_EXPIRED })
     navController.navigateRoot(Routes.LOGIN, { animationDirection: 'back' })
   }
 
@@ -49,11 +49,9 @@ export function apolloOptionsFactory(): ApolloClientOptions<unknown> {
       })
 
       const firstError = graphQLErrors[0]
-
       const errorMap: Record<string, () => void> = {
         [ErrorMessages.JWT]: clearCredentialsAndNavigateToLogin
       }
-
       errorMap[firstError.message]?.()
     }
   })
