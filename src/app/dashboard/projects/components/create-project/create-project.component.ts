4ede0a8d03050b9171484afb35a717db10e97fe9
Steven-failla_mmcgh <steven.failla@nera.com>
2024-03-06 18:03:02 -0500

    add create-project form component

--- src/app/dashboard/projects/components/create-project/create-project.component.ts
+++ src/app/dashboard/projects/components/create-project/create-project.component.ts
@@ -1,63 +1,41 @@
 import { Component } from '@angular/core'
 import { IonicModule } from '@ionic/angular'
+import { CreateProjectFormComponent, PageWrapperComponent } from '../../../../shared/components'
+import { DesktopPreviewComponent } from '../desktop-preview/desktop-preview.component'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-create-project',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, PageWrapperComponent, DesktopPreviewComponent, CreateProjectFormComponent],
   template: `
-    <div class="page-container create-project">
-      <ion-card class="create-project__card">
-        <ion-grid>
-          <ion-row>
-            <ion-col>
+    <app-page-wrapper title="Create Project">
+      <div class="create-project">
+        <ion-card class="create-project__card">
+          <div class="create-project__left-side">
+            <div class="content-container">
               <ion-card-header class="create-project__card-header">
                 <ion-card-title>New Project</ion-card-title>
               </ion-card-header>
+              <ion-card-content class="create-project__card-content">
+                <app-create-project-form [form]="form" />
+              </ion-card-content>
+            </div>
+          </div>
+          <div class="create-project__right-side">
+            <div class="background-image"></div>
+          </div>
 
-              <form action="">
-                <ion-card-content class="create-project__card-content">
-                  <ion-item>
-                    <ion-input
-                      label="Project Name"
-                      labelPlacement="floating"
-                      type="text"></ion-input>
-                  </ion-item>
-
-                  <ion-item>
-                    <ion-textarea
-                      label="Project Description"
-                      labelPlacement="floating"></ion-textarea>
-                  </ion-item>
-                </ion-card-content>
-              </form>
-            </ion-col>
-            <ion-col>placeholder</ion-col>
-          </ion-row>
-        </ion-grid>
-      </ion-card>
-    </div>
+          <app-desktop-preview></app-desktop-preview>
+        </ion-card>
+      </div>
+    </app-page-wrapper>
   `,
-  styles: [
-    `
-      @use '../../../../styles/abstracts' as *;
-
-      .create-project {
-        &__card {
-        }
-
-        &__card-header {
-          padding: 1.6rem;
-
-          & ion-card-title {
-            font-size: 2rem;
-          }
-        }
-
-        &__card-content {
-        }
-      }
-    `
-  ]
+  styleUrls: ['./create-project.component.scss']
 })
-export class CreateProjectComponent {}
+export class CreateProjectComponent {
+  form = new FormGroup({
+    projectName: new FormControl('', Validators.required),
+    projectDescription: new FormControl('')
+  })
+}
