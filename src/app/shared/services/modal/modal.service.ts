ca5469a5e064822ead312a66744cfef81f5c4fed
Steven-failla_mmcgh <steven.failla@nera.com>
2024-03-13 15:37:47 -0400

    create interface for ModalOptions. rename fns for creating, presenting and dismissing the modal

--- src/app/shared/services/modal/modal.service.ts
+++ src/app/shared/services/modal/modal.service.ts
@@ -1,24 +1,32 @@
 import { Injectable, inject, signal } from '@angular/core'
 import { OverlayService } from '../overlay/overlay.service'
 
+export interface ModalOptions {
+  title: string
+  component: any
+}
+
 @Injectable({ providedIn: 'root' })
 export class ModalService {
   overlayService: OverlayService = inject(OverlayService)
 
   showModal = signal<boolean>(false)
-  modalTitle = signal<string>('')
+  title = signal<string>('')
   component = signal<any>(null)
 
-  openModal({ title, component }: { title: string; component: any }): void {
-    this.showModal.set(true)
-    this.modalTitle.set(title)
+  create({ title, component }: ModalOptions): void {
+    this.title.set(title)
     this.component.set(component)
+  }
+
+  present(): void {
+    this.showModal.set(true)
     this.overlayService.present()
   }
 
-  closeModal(): void {
+  dismiss(): void {
     this.showModal.set(false)
-    this.modalTitle.set('')
+    this.title.set('')
     this.overlayService.dismiss()
   }
 }
