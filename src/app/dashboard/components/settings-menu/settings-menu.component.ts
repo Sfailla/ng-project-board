641136ceb0541fcd923cef2dd68662ee10b31913
Steven-failla_mmcgh <steven.failla@nera.com>
2024-03-18 09:00:47 -0400

    add primary color for settings menu slide-out header.  move styles to separate component

--- src/app/dashboard/components/settings-menu/settings-menu.component.ts
+++ src/app/dashboard/components/settings-menu/settings-menu.component.ts
@@ -10,7 +10,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
   template: `
     <ion-menu class="settings-menu" side="end" type="overlay" menuId="settings" contentId="main">
       <ion-header>
-        <ion-toolbar color="danger">
+        <ion-toolbar color="primary">
           <ion-title>Settings Menu</ion-title>
         </ion-toolbar>
       </ion-header>
@@ -28,73 +28,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
       </ion-content>
     </ion-menu>
   `,
-  styles: [
-    `
-      @use '../../../styles/abstracts' as *;
-
-      .settings-menu {
-        --width: rem(250px);
-        z-index: 1;
-
-        &:focus-visible {
-          outline: none;
-        }
-
-        & ion-header {
-          --background: var(--dashboard-sub-background);
-          height: rem(60px);
-
-          & ion-toolbar {
-            height: rem(60px);
-          }
-        }
-
-        & ion-content {
-          --background: var(--dashboard-sub-background);
-          border-left: 1px solid var(--dashboard-border-color);
-
-          & ion-list {
-            padding: 0;
-            background: var(--dashboard-sub-background);
-
-            & ion-item {
-              --background: var(--dashboard-sub-background);
-              font-size: rem(14px);
-              font-weight: bold;
-              letter-spacing: rem(0.5px);
-              color: var(--ion-color-medium-shade);
-              text-transform: uppercase;
-              cursor: pointer;
-              border-bottom: 1px solid var(--dashboard-border-color);
-
-              &::part(native) {
-                height: rem(59px);
-              }
-
-              &:first-child {
-                border-top: 1px solid var(--dashboard-border-color);
-              }
-
-              &:hover {
-                & ion-icon,
-                & ion-label {
-                  color: var(--ion-color-primary);
-                }
-              }
-
-              & ion-label {
-                margin-left: rem(5px);
-              }
-
-              & ion-icon {
-                font-size: rem(24px);
-              }
-            }
-          }
-        }
-      }
-    `
-  ]
+  styleUrl: './settings-menu.component.scss'
 })
 export class SettingsMenuComponent {
   authService: AuthService = inject(AuthService)
