6e326a82f81b7dc2c9e319e7ce0a9f5118ae4c37
Steven-failla_mmcgh <steven.failla@nera.com>
2024-04-14 15:54:36 -0400

    wip: start implementing task card and styles

--- src/app/dashboard/board/components/section/board-section.component.ts
+++ src/app/dashboard/board/components/section/board-section.component.ts
@@ -14,6 +14,37 @@ import { IonicModule } from '@ionic/angular'
           <ion-icon color="white" aria-label="add-icon" name="add" size="small" slot="icon-only" />
         </ion-button>
       </div>
+      <div class="board-section__content">
+        <ion-card class="card">
+          <div class="card__header">
+            <div class="card__tags">
+              <div class="tag">
+                <span class="tag-color"></span>
+                <span class="tag-name">ui/ux</span>
+              </div>
+              <div class="tag">
+                <span class="tag-color"></span>
+                <span class="tag-name">ui/ux</span>
+              </div>
+              <div class="tag">
+                <span class="tag-color"></span>
+                <span class="tag-name">ui/ux</span>
+              </div>
+            </div>
+            <div class="card__date">
+              <span>Apr 3</span>
+            </div>
+          </div>
+          <div class="card__title">
+            <ion-text>Design a new logo</ion-text>
+          </div>
+          <div class="card__footer">
+            <div class="card__avatar">
+              <img src="https://randomuser.me/api/portraits" alt="avatar" />
+            </div>
+          </div>
+        </ion-card>
+      </div>
     </section>
   `,
   styleUrl: './board-section.component.scss',
