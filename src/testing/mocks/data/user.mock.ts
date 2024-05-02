171e7c7b0568809f130e14dc63be4aba086bcf37
Steven-failla_mmcgh <steven.failla@nera.com>
2024-05-02 09:14:23 -0400

    create user mock

---
+++ src/testing/mocks/data/user.mock.ts
@@ -0,0 +1,13 @@
+export const mockUser = {
+  id: 'e8eca0f6-8214-47b0-b35c-92d3587956f5',
+  firstname: 'Steven',
+  lastname: 'Failla',
+  fullname: 'Steven Failla',
+  username: 'sfailla',
+  email: 'sfailla1983@gmail.com',
+  company: null,
+  position: null,
+  avatar: null,
+  createdAt: '2024-04-10T00:09:44.253Z',
+  updatedAt: '2024-04-10T00:09:44.253Z'
+}
