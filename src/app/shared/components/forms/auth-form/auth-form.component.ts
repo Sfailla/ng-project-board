bc145c9d4660c93f2aab38252ddee28629f0bb31
Steven-failla_mmcgh <steven.failla@nera.com>
2024-05-03 12:38:41 -0400

    move submit fn to auth-form.  add style.component and moved styles

--- src/app/shared/components/forms/auth-form/auth-form.component.ts
+++ src/app/shared/components/forms/auth-form/auth-form.component.ts
@@ -1,14 +1,16 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, input } from '@angular/core'
 import { FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { IonicModule } from '@ionic/angular'
-import { AuthTitles } from '../../../../auth/auth-types'
+import { AuthService } from '@auth/services'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { AuthTitles, AuthUserInput } from '../../../../auth/auth-types'
 
 @Component({
   selector: 'app-auth-form',
   standalone: true,
   imports: [IonicModule, ReactiveFormsModule],
   template: `
-    <form [formGroup]="form()" (ngSubmit)="submitForm()">
+    <form [formGroup]="form()" (ngSubmit)="submit()">
       <ion-list lines="none">
         @if (!isLogin()) {
           <ion-item color="light">
@@ -57,58 +59,23 @@ import { AuthTitles } from '../../../../auth/auth-types'
       </ion-button>
     </form>
   `,
-  styles: [
-    `
-      @use '../../../../styles/abstracts' as *;
-
-      form {
-        & ion-list {
-          background-color: var(--ion-color-light);
-        }
-
-        & ion-item {
-          border: 1px solid var(--form-border-color);
-          border-radius: rem(5px);
-          height: rem(50px);
-
-          &:not(:last-of-type) {
-            margin-bottom: rem(15px);
-          }
-
-          & ion-input {
-            font-size: rem(13px);
-            transform: translateY(-2px) scaleY(0.9);
-
-            & input {
-              padding-bottom: rem(6px);
-            }
-          }
-        }
-
-        & ion-button {
-          height: rem(50px);
-          margin-top: rem(30px);
-          font-size: rem(14px);
-
-          &:disabled {
-            opacity: 0.5;
-
-            &::after {
-              display: none;
-            }
-          }
-        }
-      }
-    `
-  ],
+  styleUrl: 'style.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuthFormComponent {
   isLogin = input.required<boolean>()
   form = input.required<FormGroup>()
-  submit = input.required<() => void>()
 
-  submitForm = () => this.submit()()
+  authService: AuthService = inject(AuthService)
+  destroyRef: DestroyRef = inject(DestroyRef)
 
   AuthTitles: typeof AuthTitles = AuthTitles
+
+  submit() {
+    const credentials = <AuthUserInput>this.form().value
+
+    this.isLogin()
+      ? this.authService.login(credentials).pipe(takeUntilDestroyed(this.destroyRef)).subscribe()
+      : this.authService.register(credentials).pipe(takeUntilDestroyed(this.destroyRef)).subscribe()
+  }
 }
