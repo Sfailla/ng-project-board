b96320846eef7891273880756cb82659749d3998
Steven-failla_mmcgh <steven.failla@nera.com>
2024-05-03 19:43:10 -0400

    create login component test

---
+++ src/app/auth/components/login/login.component.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing'
+import { LoginComponent } from './login.component'
+import { MockAuthService } from '@testing/mocks/services'
+import { Apollo } from 'apollo-angular'
+import { AuthService } from '@auth/services'
+import { provideRouter } from '@angular/router'
+
+describe('LoginComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoginComponent],
+      providers: [
+        provideRouter([]),
+        { provide: Apollo, useValue: {} },
+        { provide: AuthService, useClass: MockAuthService }
+      ]
+    }).compileComponents()
+  })
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(LoginComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+})
