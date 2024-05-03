b0d5b10106c3bb67f9297923aed7ddfc1dc2615e
Steven-failla_mmcgh <steven.failla@nera.com>
2024-05-03 19:44:52 -0400

    change import for auth service to alias

--- src/app/auth/guards/auth-route-lock.guard.ts
+++ src/app/auth/guards/auth-route-lock.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core'
 import { CanActivateFn, Router } from '@angular/router'
-import { AuthService } from '../services/auth/auth.service'
+import { AuthService } from '@auth/services'
 import { CanActivateReturnType } from '../auth-types'
 
 export const authRouteLockGuard: CanActivateFn = (): CanActivateReturnType => {
