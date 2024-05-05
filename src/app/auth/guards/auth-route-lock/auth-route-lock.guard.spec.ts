6bc3a072743c2170719cd15662b2eb882a8a3127
Steven-failla_mmcgh <steven.failla@nera.com>
2024-05-05 13:03:55 -0400

    add test cases for guards

---
+++ src/app/auth/guards/auth-route-lock/auth-route-lock.guard.spec.ts
@@ -0,0 +1,55 @@
+import { ActivatedRoute, Router, RouterStateSnapshot, UrlTree } from '@angular/router'
+import { authRouteLockGuard } from './auth-route-lock.guard'
+import { TestBed } from '@angular/core/testing'
+import { AuthService } from '../../services'
+import { Apollo } from 'apollo-angular'
+import { Routes } from '../../../shared/types'
+
+describe('AuthRouteLockGuard', () => {
+  let authService: AuthService
+  let router: Router
+  let activatedRoute: ActivatedRoute
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        { provide: ActivatedRoute, useValue: { snapshot: {} } },
+        { provide: Apollo, useValue: {} }
+      ]
+    }).compileComponents()
+
+    authService = TestBed.inject(AuthService)
+    router = TestBed.inject(Router)
+    activatedRoute = TestBed.inject(ActivatedRoute)
+  })
+  it('should create', () => {
+    expect(authRouteLockGuard).toBeTruthy()
+  })
+
+  it('should return false if the user is authenticated', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(false)
+    jest.spyOn(router, 'createUrlTree')
+
+    const result = TestBed.runInInjectionContext(() => {
+      return authRouteLockGuard(activatedRoute.snapshot, <RouterStateSnapshot>{})
+    })
+
+    expect(result).toBe(false)
+    expect(authService.isAuthenticated).toHaveBeenCalled()
+    expect(router.createUrlTree).not.toHaveBeenCalled()
+  })
+
+  it('should return a url tree if the user is authenticated', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(true)
+    jest.spyOn(router, 'createUrlTree')
+
+    const result = TestBed.runInInjectionContext(() => {
+      return authRouteLockGuard(activatedRoute.snapshot, <RouterStateSnapshot>{})
+    })
+
+    expect(result).toBeInstanceOf(UrlTree)
+    expect(authService.isAuthenticated).toHaveBeenCalled()
+    expect(router.createUrlTree).toHaveBeenCalled()
+    expect(router.createUrlTree).toHaveBeenCalledWith([Routes.LOGIN])
+  })
+})
---
+++ src/app/auth/guards/auth/auth.guard.spec.ts
@@ -0,0 +1,55 @@
+import { ActivatedRoute, Router, RouterStateSnapshot, UrlTree } from '@angular/router'
+import { authGuard } from './auth.guard'
+import { TestBed } from '@angular/core/testing'
+import { AuthService } from '../../services'
+import { Apollo } from 'apollo-angular'
+import { Routes } from '../../../shared/types'
+
+describe('AuthGuard', () => {
+  let authService: AuthService
+  let router: Router
+  let activatedRoute: ActivatedRoute
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        { provide: ActivatedRoute, useValue: { snapshot: {} } },
+        { provide: Apollo, useValue: {} }
+      ]
+    }).compileComponents()
+
+    authService = TestBed.inject(AuthService)
+    router = TestBed.inject(Router)
+    activatedRoute = TestBed.inject(ActivatedRoute)
+  })
+  it('should create', () => {
+    expect(authGuard).toBeTruthy()
+  })
+
+  it('should return true if the user is authenticated', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(true)
+    jest.spyOn(router, 'createUrlTree')
+
+    const result = TestBed.runInInjectionContext(() => {
+      return authGuard(activatedRoute.snapshot, <RouterStateSnapshot>{})
+    })
+
+    expect(result).toBe(true)
+    expect(authService.isAuthenticated).toHaveBeenCalled()
+    expect(router.createUrlTree).not.toHaveBeenCalled()
+  })
+
+  it('should return a url tree if the user is not authenticated', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(false)
+    jest.spyOn(router, 'createUrlTree')
+
+    const result = TestBed.runInInjectionContext(() => {
+      return authGuard(activatedRoute.snapshot, <RouterStateSnapshot>{})
+    })
+
+    expect(result).toBeInstanceOf(UrlTree)
+    expect(authService.isAuthenticated).toHaveBeenCalled()
+    expect(router.createUrlTree).toHaveBeenCalled()
+    expect(router.createUrlTree).toHaveBeenCalledWith([Routes.LOGIN])
+  })
+})
