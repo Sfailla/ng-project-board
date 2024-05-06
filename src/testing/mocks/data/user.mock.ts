013533f27f715088012a35ac496479ba5a56bb33
Steven-failla_mmcgh <steven.failla@nera.com>
2024-05-06 09:15:09 -0400

    add mock token

--- src/testing/mocks/data/user.mock.ts
+++ src/testing/mocks/data/user.mock.ts
@@ -11,3 +11,6 @@ export const mockUser = {
   createdAt: '2024-04-10T00:09:44.253Z',
   updatedAt: '2024-04-10T00:09:44.253Z'
 }
+
+export const mockToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoLXNlcnZpY2UiLCJhdWQiOiJjbGllbnQtc2VydmljZSIsInVzZXIiOnsiaWQiOiJlOGVjYTBmNi04MjE0LTQ3YjAtYjM1Yy05MmQzNTg3OTU2ZjUiLCJ1c2VybmFtZSI6InNmYWlsbGEiLCJlbWFpbCI6InNmYWlsbGExOTgzQGdtYWlsLmNvbSJ9LCJpYXQiOjE3MTQzMzkwNDksImV4cCI6MTcxNDk0Mzg0OX0.D0QT1Bv0xI7jEwhe9EUxsf5u5w7AaSzTVQ2imk1vwLs'
