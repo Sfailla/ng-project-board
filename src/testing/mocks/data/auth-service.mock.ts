b2cc4b94dca47c6dfa2f12c04e9c006219bdd23e
Steven-failla_mmcgh <steven.failla@nera.com>
2024-05-08 20:19:08 -0400

    add mocks and helper fns for auth service mock

--- src/testing/mocks/data/auth-service.mock.ts
+++ src/testing/mocks/data/auth-service.mock.ts
@@ -1,17 +1,24 @@
 import { GraphQLError } from 'graphql'
 import { mockUser, mockToken } from './'
 import { AuthUserInput } from '../../../app/auth/auth-types'
-import { withData, withErrors } from '@testing/utils'
+import { withMutateData, withMutateErrors, withQueryData } from '@testing/utils'
 import { ErrorMessages } from '@shared/types'
 
-type AuthInputOptions = { type?: 'login' | 'register' }
+type AuthInputOptions = {
+  type?: 'login' | 'register'
+  updateKey?: Partial<AuthUserInput>
+}
 
 const loginCredentials = { email: 'sfailla@gmail.com', password: '1234' }
+const registerCredentials = { username: 'sfailla', confirmPassword: '1234' }
 
-export const getAuthUserInput = ({ type = 'login' }: AuthInputOptions = {}): AuthUserInput => {
+export const getAuthUserInput = ({
+  type = 'login',
+  updateKey
+}: AuthInputOptions = {}): AuthUserInput => {
   return type === 'login'
-    ? loginCredentials
-    : { ...loginCredentials, username: 'sfailla', confirmPassword: '1234' }
+    ? { ...loginCredentials, ...updateKey }
+    : { ...loginCredentials, ...registerCredentials, ...updateKey }
 }
 
 export const baseAuthenticatedResponse = {
@@ -19,14 +26,24 @@ export const baseAuthenticatedResponse = {
   token: mockToken
 }
 
-export const mockLoginResponseWithData = withData({
+export const mockLoginResponseWithData = withMutateData({
   login: baseAuthenticatedResponse
 })
 
-export const mockRegisterResponseWithData = withData({
+export const mockRegisterResponseWithData = withMutateData({
   createUser: baseAuthenticatedResponse
 })
 
-export const mockLoginResponseWithError = withErrors(<GraphQLError[]>[
+export const mockLoginResponseWithError = withMutateErrors(<GraphQLError[]>[
   { message: ErrorMessages.LOGIN_FAILED }
 ])
+
+export const mockRegisterResponseWithError = withMutateErrors(<GraphQLError[]>[
+  { message: ErrorMessages.REGISTRATION_FAILED }
+])
+
+export const mockLogoutResponseWithData = withQueryData({
+  logout: {
+    user: null
+  }
+})
