61d45b89b26cb093f0c937ed2c9d197bcaac4b7b
Steven-failla_mmcgh <steven.failla@nera.com>
2024-05-30 18:39:11 -0400

    improve typing for additonalProvider value

--- src/testing/utils/test-setup.ts
+++ src/testing/utils/test-setup.ts
@@ -3,16 +3,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 export type InputSignal = Record<string, any>
 
-export type AdditionalProvider<K> = { name: K; value: any }
+export type AdditionalProvider<K, V> = { name: K; value: Type<V> }
 
-export type SetupTestOptions<K extends string> = {
+export type SetupTestOptions<K extends string, V> = {
   setInput?: InputSignal
-  additionalProviders?: AdditionalProvider<K>[]
+  additionalProviders?: AdditionalProvider<K, V>[]
 }
 
-export function setupTest<T extends object, K extends string>(
+export function setupTest<T extends object, K extends string, V>(
   Component: Type<T>,
-  options: SetupTestOptions<K> = {}
+  options: SetupTestOptions<K, V> = {}
 ) {
   const fixture: ComponentFixture<T> = TestBed.createComponent(Component)
   const component: T = fixture.componentInstance
@@ -24,7 +24,7 @@ export function setupTest<T extends object, K extends string>(
     })
   }
 
-  const providers = <Record<K, any>>{}
+  const providers = <Record<K, V>>{}
 
   if (options.additionalProviders) {
     options.additionalProviders.forEach(({ name, value }) => {
