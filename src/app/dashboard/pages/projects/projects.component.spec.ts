ba2e68bbfa7f6b4a7a25f600496f4b5057466a38
Steven-failla_mmcgh <steven.failla@nera.com>
2024-06-05 09:29:22 -0400

    add test setup for projects component

--- src/app/dashboard/pages/projects/projects.component.spec.ts
+++ src/app/dashboard/pages/projects/projects.component.spec.ts
@@ -1,13 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { TestBed } from '@angular/core/testing'
 import { ProjectsComponent } from './projects.component'
 import { ProjectService } from '@shared/services'
 import { RouterTestingModule } from '@angular/router/testing'
 import { of } from 'rxjs/internal/observable/of'
+import { setupTest } from '@testing/utils'
 
 describe('ProjectsComponent', () => {
-  let component: ProjectsComponent
-  let fixture: ComponentFixture<ProjectsComponent>
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProjectsComponent, RouterTestingModule],
@@ -18,13 +16,10 @@ describe('ProjectsComponent', () => {
         }
       ]
     }).compileComponents()
-
-    fixture = TestBed.createComponent(ProjectsComponent)
-    component = fixture.componentInstance
-    fixture.detectChanges()
   })
 
   it('should create', () => {
+    const { component } = setupTest(ProjectsComponent)
     expect(component).toBeTruthy()
   })
 })
