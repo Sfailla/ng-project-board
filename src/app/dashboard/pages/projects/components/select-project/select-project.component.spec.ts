f4cb2b43b0258214aa58550f361a3e7e07391c8e
Steven-failla_mmcgh <steven.failla@nera.com>
2024-06-14 07:59:40 -0400

    add test for select-project component

--- src/app/dashboard/pages/projects/components/select-project/select-project.component.spec.ts
+++ src/app/dashboard/pages/projects/components/select-project/select-project.component.spec.ts
@@ -3,7 +3,7 @@ import { TestBed } from '@angular/core/testing'
 import { SelectProjectComponent } from './select-project.component'
 import { ProjectService } from '@shared/services'
 import { RouterTestingModule } from '@angular/router/testing'
-import { debug, setupTest } from '@testing/utils'
+import { setupTest } from '@testing/utils'
 import { ProjectServiceMock } from '@testing/mocks/services'
 import { Apollo } from 'apollo-angular'
 
@@ -22,8 +22,12 @@ describe('SelectProjectComponent', () => {
   })
 
   it('should create', () => {
-    const { fixture, component } = setupTest(SelectProjectComponent)
-    debug(fixture)
+    const { component } = setupTest(SelectProjectComponent)
     expect(component).toBeTruthy()
+  })
+
+  it('should get 3 projects and display on page', () => {
+    const { component } = setupTest(SelectProjectComponent)
+    expect(component.projects()).toHaveLength(3)
   })
 })
