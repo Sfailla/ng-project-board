190f72decd42d426033e73040a465b45ef5d87b3
Steven-failla_mmcgh <steven.failla@nera.com>
2024-06-14 08:00:00 -0400

    add mock data for project service data calls

--- src/testing/mocks/data/project-service.mock.ts
+++ src/testing/mocks/data/project-service.mock.ts
@@ -121,3 +121,129 @@ export const mockProjectsResponseWithData = withQueryData({
     }
   ]
 })
+
+export const mockGetProjectByIdResponseWithData = withQueryData({
+  project: {
+    id: 'fa18c8dd-f6d8-4c1a-95c8-dcae4a0bd69f',
+    name: 'Project 1',
+    description: 'This is a description for Project 1',
+    dueDate: null,
+    categories: [
+      {
+        id: '4f3f7662-d63a-4608-9490-db27593424bd',
+        name: 'Open',
+        status: 'open',
+        displayOrder: 0
+      },
+      {
+        id: '9cb58444-b89d-420a-8a59-0ff3e293018c',
+        name: 'In Progress',
+        status: 'in-progress',
+        displayOrder: 1
+      },
+      {
+        id: '1fdddf46-23c7-4183-8ede-c7bb340d9c1f',
+        name: 'In Review',
+        status: 'in-review',
+        displayOrder: 2
+      },
+      {
+        id: '2b43dd7c-ab24-41b1-9836-f4e536ac4ca3',
+        name: 'Complete',
+        status: 'complete',
+        displayOrder: 3
+      }
+    ],
+    user: {
+      id: 'e8eca0f6-8214-47b0-b35c-92d3587956f5',
+      email: 'sfailla1983@gmail.com',
+      username: 'sfailla'
+    },
+    createdAt: '2024-04-10T01:55:59.697Z',
+    updatedAt: '2024-04-10T01:55:59.697Z'
+  }
+})
+
+export const mockCreateProjectResponseWithData = withQueryData({
+  createProject: {
+    id: 'fa18c8dd-f6d8-4c1a-95c8-dcae4a0bd69f',
+    name: 'Project 1',
+    description: 'This is a description for Project 1',
+    dueDate: null,
+    categories: [
+      {
+        id: '4f3f7662-d63a-4608-9490-db27593424bd',
+        name: 'Open',
+        status: 'open',
+        displayOrder: 0
+      },
+      {
+        id: '9cb58444-b89d-420a-8a59-0ff3e293018c',
+        name: 'In Progress',
+        status: 'in-progress',
+        displayOrder: 1
+      },
+      {
+        id: '1fdddf46-23c7-4183-8ede-c7bb340d9c1f',
+        name: 'In Review',
+        status: 'in-review',
+        displayOrder: 2
+      },
+      {
+        id: '2b43dd7c-ab24-41b1-9836-f4e536ac4ca3',
+        name: 'Complete',
+        status: 'complete',
+        displayOrder: 3
+      }
+    ],
+    user: {
+      id: 'e8eca0f6-8214-47b0-b35c-92d3587956f5',
+      email: 'sfailla1983@gmail.com',
+      username: 'sfailla'
+    },
+    createdAt: '2024-04-10T01:55:59.697Z',
+    updatedAt: '2024-04-10T01:55:59.697Z'
+  }
+})
+
+export const mockUpdateProjectResponseWithData = withQueryData({
+  updateProject: {
+    id: 'fa18c8dd-f6d8-4c1a-95c8-dcae4a0bd69f',
+    name: 'Project 1',
+    description: 'This is a description for Project 1',
+    dueDate: null,
+    categories: [
+      {
+        id: '4f3f7662-d63a-4608-9490-db27593424bd',
+        name: 'Open',
+        status: 'open',
+        displayOrder: 0
+      },
+      {
+        id: '9cb58444-b89d-420a-8a59-0ff3e293018c',
+        name: 'In Progress',
+        status: 'in-progress',
+        displayOrder: 1
+      },
+      {
+        id: '1fdddf46-23c7-4183-8ede-c7bb340d9c1f',
+        name: 'In Review',
+        status: 'in-review',
+        displayOrder: 2
+      },
+      {
+        id: '2b43dd7c-ab24-41b1-9836-f4e536ac4ca3',
+        name: 'Complete',
+        status: 'complete',
+        displayOrder: 3
+      }
+    ],
+    user: {
+      id: 'e8eca0f6-8214-47b0-b35c-92d3587956f5',
+      email: 'sfailla1983@gmail.com',
+      username: 'sfailla'
+    },
+    createdAt: '2024-04-10T01:55:59.697Z',
+    updatedAt: '2024-04-10T01:55:59.697Z'
+  }
+})
