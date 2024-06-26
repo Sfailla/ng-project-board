c5437d72ba002e31beb90e084bffe0c3a31c67b1
Steven-failla_mmcgh <steven.failla@nera.com>
2024-06-26 14:44:02 -0400

    add test for create-project button component

---
+++ src/app/dashboard/pages/projects/components/buttons/create-project-button/create-project-button.component.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed } from '@angular/core/testing'
+import { CreateProjectButtonComponent } from './create-project-button.component'
+import { debug, findNativeElement, setupTest } from '@testing/utils'
+import { Router, provideRouter } from '@angular/router'
+
+describe('CreateProjectButtonComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CreateProjectButtonComponent],
+      providers: [
+        provideRouter([
+          { path: 'dashboard/projects/create', component: CreateProjectButtonComponent }
+        ])
+      ]
+    }).compileComponents()
+  })
+
+  it('should create', () => {
+    const { fixture, component } = setupTest(CreateProjectButtonComponent)
+    debug(fixture)
+    expect(component).toBeTruthy()
+  })
+
+  it('should route to create-project on button click', async () => {
+    const { fixture, router } = setupTest(CreateProjectButtonComponent, {
+      additionalProviders: [{ name: 'router', value: Router }]
+    })
+
+    const button = findNativeElement(fixture, 'a')
+    button.click()
+
+    expect(router.url).toBe('/dashboard/projects/create')
+  })
+})
