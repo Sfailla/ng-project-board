be8b4dd7cfeab1cc5e49d16ed857a8c6f830478b
Steven-failla_mmcgh <steven.failla@nera.com>
2024-06-26 15:20:34 -0400

    add project service mock to providers.  remove debug statement

--- src/app/dashboard/pages/projects/projects.component.spec.ts
+++ src/app/dashboard/pages/projects/projects.component.spec.ts
@@ -2,19 +2,15 @@ import { TestBed } from '@angular/core/testing'
 import { ProjectsComponent } from './projects.component'
 import { ProjectService } from '@shared/services'
 import { RouterTestingModule } from '@angular/router/testing'
-import { of } from 'rxjs/internal/observable/of'
 import { setupTest } from '@testing/utils'
+import { ProjectServiceMock } from '@testing/mocks/services'
+import { Apollo } from 'apollo-angular'
 
 describe('ProjectsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProjectsComponent, RouterTestingModule],
-      providers: [
-        {
-          provide: ProjectService,
-          useValue: { getProjectId: () => null, getProjects: () => of([]) }
-        }
-      ]
+      providers: [Apollo, { provide: ProjectService, useClass: ProjectServiceMock }]
     }).compileComponents()
   })
 
