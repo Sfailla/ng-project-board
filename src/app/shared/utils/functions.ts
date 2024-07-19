3fd4456fa32f8097600c19e787071c5dfdca3872
Steven-failla_mmcgh <steven.failla@nera.com>
2024-07-19 16:31:48 -0400

    improve type inference for util getDataAndSetSignals fn

--- src/app/shared/utils/functions.ts
+++ src/app/shared/utils/functions.ts
@@ -1,11 +1,8 @@
 import { Observable } from 'rxjs/internal/Observable'
 import { map } from 'rxjs/internal/operators/map'
 
-export type SetSignals<T extends object> = (state: T) => void
+export type SetSignals<T> = (state: T) => void
 
-export const getDataAndSetSignals = <T extends Observable<object>, S extends object>(
-  observable: T,
-  setSignalsFn: SetSignals<S>
-) => {
-  return observable.pipe(map(data => setSignalsFn(data as S)))
+export const getDataAndSetSignals = <S>(observable: Observable<S>, setSignalsFn: SetSignals<S>) => {
+  return observable.pipe(map(data => setSignalsFn(data)))
 }
